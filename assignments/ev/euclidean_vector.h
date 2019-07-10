db7362948c7f6973efc912b6faafe5f68c7a0db6
quinn <z5117408@unsw.edu.au>
2019-07-11 00:00:51 +1000

    done most of interface i.e. .h file

--- assignments/ev/euclidean_vector.h
+++ assignments/ev/euclidean_vector.h
@@ -1,8 +1,10 @@
 // TODO(you): Include header guards
 
 #include <exception>
+#include <list>
 #include <string>
 #include <memory>
+#include <vector>
 
 class EuclideanVectorError : public std::exception {
  public:
@@ -14,9 +16,48 @@ class EuclideanVectorError : public std::exception {
 
 class EuclideanVector {
  public:
-  explicit EuclideanVector(int i);
-  friend std::ostream& operator<<(std::ostream& os, const EuclideanVector& v);
+  /* ctors */
+  explicit EuclideanVector(int dimension = 1); // default dimension 1 ctor (i dimensions, all magnitudes 0)
+  EuclideanVector(int dimension, double magnitude); // i dimensions, all magnitudes double
+  EuclideanVector(std::vector<double>::const_iterator begin, std::vector<double>::const_iterator end); // iterate over vector
+  EuclideanVector(const EuclideanVector& original); // copy construct
+  EuclideanVector(EuclideanVector&& original) noexcept; // move construct ((1) noexcept as a failed move does not guarantee valid state)
+
+  /* dtor */
+  ~EuclideanVector() noexcept = default; // TODO: is this correct?
+
+  /* operator overloads */
+  EuclideanVector& operator=(const EuclideanVector& original); // copy assign
+  EuclideanVector& operator=(EuclideanVector&& recyclee) noexcept; // move assign (see (1))
+  double& operator[](int i); // return reference to value at index i (TODO: is double correct?)
+  double operator[](int i) const; // return value at index i (TODO: is double correct?)
+  EuclideanVector& operator+=(const EuclideanVector& other);
+  EuclideanVector& operator-=(const EuclideanVector& other);
+  EuclideanVector& operator*=(const double scaler);
+  EuclideanVector& operator/=(const double scaler);
+  explicit operator std::vector<double>();
+  explicit operator std::list<double>();
+
+  /* friend operator overloads */
+  friend bool operator==(const EuclideanVector& a, const EuclideanVector& b);
+  friend bool operator!=(const EuclideanVector& a, const EuclideanVector& b); // NOTE: this is just inverse of above, re-use code 
+  friend EuclideanVector operator+(const EuclideanVector& a, const EuclideanVector& b);
+  friend EuclideanVector operator-(const EuclideanVector& a, const EuclideanVector& b);
+  friend double operator*(const EuclideanVector& a, const EuclideanVector& b);
+  friend EuclideanVector operator*(const EuclideanVector& ev, double scaler);
+  friend EuclideanVector operator/(const EuclideanVector& ev, double scaler);
+  friend std::ostream& operator<<(std::ostream& os, const EuclideanVector& ev);
+
+  /* methods */
+  double at(int i) const;
+  double& at(int i);
+  int GetNumDimensions();
+  double GetEuclideanNorm();
+  EuclideanVector CreateUnitVector();
+
+  
   // TODO(you): add more
+ 
  private:
   std::unique_ptr<double[]> magnitudes_;
   // TODO(you): add more
