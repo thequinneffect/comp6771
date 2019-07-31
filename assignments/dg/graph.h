a2371a7abff1b69b271abd749064b18926a09d50
quinn <z5117408@unsw.edu.au>
2019-08-01 00:07:23 +1000

    design is almost done

--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -1,8 +1,11 @@
 #ifndef ASSIGNMENTS_DG_GRAPH_H_
 #define ASSIGNMENTS_DG_GRAPH_H_
 
+#include <experimental/memory>
+#include <map>
 #include <memory>
-#include <vector>
+#include <unordered_set>
+#include <unordered_map>
 
 namespace gdwg {
 
@@ -10,7 +13,6 @@ template <typename N, typename E> // could be for example "string, int", "int, d
 class Graph {
   public:
     class const_iterator {};
-    
 
   private:
 
@@ -20,9 +22,11 @@ class Graph {
      * the resource. I chose to solely store observer_ptrs to Edge
      * objects rather than other Nodes because an Edge has to store
      * the src->dst relationship anyway. */
-    class Node {
-        std::vector<std::observer_ptr<Edge>> incoming_; // 0..*
-        std::vector<std::observer_ptr<Edge>> outgoing_; // 0..*
+    struct Node {
+        /* i've chosen sets rather than vectors so that removal is
+         * nicer in that we don't have wasted slots */
+        std::unordered_set<std::weak_ptr<Edge>> incoming_; // 0..*
+        std::unordered_set<std::weak_ptr<Edge>> outgoing_; // 0..*
         N value_; // exactly 1
     };
 
@@ -31,18 +35,28 @@ class Graph {
      * owned by a unique_ptr (for same reasons as above). Each edge
      * also has exactly one src Node and dst Node, and because these
      * resources are already owned by unique_ptr's, I just use 
-     * observer_ptrs as references (they can re-point when the 
-     * underlying heap memory moves, refs cannot) */
-    class Edge {
-        std::observer_ptr<Node> src_; // exactly 1
-        std::observer_ptr<Node> dst_; // exactly 1
+     * observer_ptrs */
+    struct Edge {
+        std::experimental::observer_ptr<Node> src_; // exactly 1
+        std::experimental::observer_ptr<Node> dst_; // exactly 1
         E weight_; // exactly 1
     };
 
     /* owns all nodes */
-    std::vector<std::unique_ptr<Node>> nodes_;
+    /* By storing the nodes in a map which is indexed by the node
+     * value N, we can look-up whether or not a node exists in
+     * the graph in amortised constant time. 
+     * - ordered so that GetNodes is easy */
+    std::map<N, std::unique_ptr<Node>> nodes_;
+    //std::vector<std::unique_ptr<Node>> nodes_;
     /* owns all edges */
-    std::vector<std::unique_ptr<Edge>> edges_;
+    /* - Map of N to Edge so that InsertEdge is easy
+     * By storing the edges in a map which is indexed by the source
+     * node, we only need to check whether or not the source node
+     * exists or not (amortised constant look-up), and then check 
+     * it's edges if it does (linear in number of outgoing edges) */
+    std::unordered_map<N, std::shared_ptr<Edge>> edges_;
+    //std::vector<std::unique_ptr<Edge>> edges_;
     
 };
 
